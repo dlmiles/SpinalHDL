f6b7dd137a053bef4b1e6ee9e3c902f249966245
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-05-12 12:38:42 +0100

    tester: DebugOut#setDefinitionName("unamed") typo fix
    project: tester
       risk: med (3/5)
       type: change of BlackBox definition label
      where: DebugInOut extends App
       what: string label change in scratch-code
        how: unamed => unnamed
     impact: low 1/5 (code not expected to be executed by users)

--- tester/src/test/scala/spinal/tester/code/Debug.scala
+++ tester/src/test/scala/spinal/tester/code/Debug.scala
@@ -40,7 +40,7 @@ object DebugInOut extends App{
       val subIo = inout(Analog(Bits(8 bits)))
     }
     io := sub.subIo
-    setDefinitionName("unamed")
+    setDefinitionName("unnamed")
   })
 }
 
