5c820ce07d6f70da459f8ffa4f4a5ef76b3f2084
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-05-14 22:44:20 +0100

    tester/python: assignment <= is deprecated: MultiClockTester/MultiClockTester.py

--- tester/src/test/python/spinal/MultiClockTester/MultiClockTester.py
+++ tester/src/test/python/spinal/MultiClockTester/MultiClockTester.py
@@ -15,12 +15,12 @@ class Packet:
 @cocotb.coroutine
 def cmd(dut,queue):
     validRandomizer = BoolRandomizer()
-    dut.io_slave0_valid <= 0
+    dut.io_slave0_valid.value = 0
     while True:
         yield RisingEdge(dut.io_clkA)
         if int(dut.io_slave0_valid) == 1 and int(dut.io_slave0_ready) == 1:
             queue.put(Packet(int(dut.io_slave0_payload_a),int(dut.io_slave0_payload_b)))
-        dut.io_slave0_valid <= validRandomizer.get()
+        dut.io_slave0_valid.value = validRandomizer.get()
         randSignal(dut.io_slave0_payload_a)
         randSignal(dut.io_slave0_payload_b)
 
@@ -29,11 +29,11 @@ def cmd(dut,queue):
 @cocotb.coroutine
 def rsp(dut,queue):
     readyRandomizer = BoolRandomizer()
-    dut.io_master0_ready <= 0
+    dut.io_master0_ready.value = 0
     for i in range(0,1000):
         while True:
             yield RisingEdge(dut.io_clkB)
-            dut.io_master0_ready <= readyRandomizer.get()
+            dut.io_master0_ready.value = readyRandomizer.get()
             if int(dut.io_master0_valid) == 1 and int(dut.io_master0_ready) == 1:
                 break
         pop = queue.get()
@@ -47,21 +47,21 @@ def rsp(dut,queue):
 @cocotb.coroutine
 def clockProcess(dut):
     randomizer = BoolRandomizer()
-    dut.io_clkA <= 0
-    dut.io_clkB <= 0
-    dut.io_resetB <= 1
-    dut.io_resetA <= 1
+    dut.io_clkA.value = 0
+    dut.io_clkB.value = 0
+    dut.io_resetB.value = 1
+    dut.io_resetA.value = 1
     yield Timer(1000)
-    dut.io_resetA <= 0
-    dut.io_resetB <= 0
+    dut.io_resetA.value = 0
+    dut.io_resetB.value = 0
     while True:
-        dut.io_clkA <= 0
-        dut.io_clkB <= 0
+        dut.io_clkA.value = 0
+        dut.io_clkB.value = 0
         yield Timer(500)
         if randomizer.get():
-            dut.io_clkA <= 1
+            dut.io_clkA.value = 1
         else:
-            dut.io_clkB <= 1
+            dut.io_clkB.value = 1
         yield Timer(500)
 
 
