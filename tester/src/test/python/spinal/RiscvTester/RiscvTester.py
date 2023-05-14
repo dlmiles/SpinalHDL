0ae2acd82074233210cca7c1d0fe9c1dd1593d51
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-05-14 22:44:40 +0100

    tester/python: assignment <= is deprecated: RiscvTester/RiscvTester.py

--- tester/src/test/python/spinal/RiscvTester/RiscvTester.py
+++ tester/src/test/python/spinal/RiscvTester/RiscvTester.py
@@ -39,26 +39,26 @@ class Tester:
     @cocotb.coroutine
     def driveICmdReady(self):
         dut = self.dut
-        dut.io_i_cmd_ready <= 1
+        dut.io_i_cmd_ready.value = 1
         while True:
             yield [Edge(dut.io_i_rsp_valid),Edge(dut.io_i_rsp_ready)]
-            dut.io_i_cmd_ready <= (int(dut.io_i_rsp_valid) == 0 or int(dut.io_i_rsp_ready) == 1);
+            dut.io_i_cmd_ready.value = (int(dut.io_i_rsp_valid) == 0 or int(dut.io_i_rsp_ready) == 1);
 
     @cocotb.coroutine
     def driveDCmdReady(self):
         dut = self.dut
-        dut.io_d_cmd_ready <= 1
+        dut.io_d_cmd_ready.value = 1
         while True:
             yield [Edge(dut.io_d_rsp_valid),Edge(dut.io_d_rsp_ready)]
-            dut.io_d_cmd_ready <= (int(dut.io_d_rsp_valid) == 0 or int(dut.io_d_rsp_ready) == 1);
+            dut.io_d_cmd_ready.value = (int(dut.io_d_rsp_valid) == 0 or int(dut.io_d_rsp_ready) == 1);
 
     @cocotb.coroutine
     def driveMisc(self):
-        self.dut.io_iCmdDrive <= 1
-        self.dut.io_iRspDrive <= 1
-        self.dut.io_dCmdDrive <= 1
-        self.dut.io_dRspDrive <= 1
-        self.dut.io_doCacheFlush <= 0
+        self.dut.io_iCmdDrive.value = 1
+        self.dut.io_iRspDrive.value = 1
+        self.dut.io_dCmdDrive.value = 1
+        self.dut.io_dRspDrive.value = 1
+        self.dut.io_doCacheFlush.value = 0
         cocotb.fork(self.driveICmdReady())
         cocotb.fork(self.driveDCmdReady())
         while True:
@@ -72,47 +72,47 @@ class Tester:
     @cocotb.coroutine
     def driveIRsp(self):
         dut = self.dut
-        dut.io_i_rsp_valid <= 0
+        dut.io_i_rsp_valid.value = 0
         while True:
             yield RisingEdge(dut.clk)
             if int(dut.io_i_rsp_ready) == 1:
-              dut.io_i_rsp_valid <= 0
+              dut.io_i_rsp_valid.value = 0
               randSignal(dut.io_i_rsp_payload_instruction)
 
             if int(dut.io_i_cmd_valid) == 1 and int(dut.io_i_cmd_ready) == 1:
-              dut.io_i_rsp_valid <= 1
-              dut.io_i_rsp_payload_pc <= int(dut.io_i_cmd_payload_pc)
+              dut.io_i_rsp_valid.value = 1
+              dut.io_i_rsp_payload_pc.value = int(dut.io_i_cmd_payload_pc)
               data = 0
-              if int(dut.io_i_cmd_payload_pc) <= 0x03FFFFFF :
+              if int(dut.io_i_cmd_payload_pc.value) <= 0x03FFFFFF :
                 for i in range(0,4):
                   data = data | (self.rom[int(dut.io_i_cmd_payload_pc) + i] << (i*8))
 
-              elif int(dut.io_i_cmd_payload_pc) <= 0x04007FFF :
+              elif int(dut.io_i_cmd_payload_pc.value) <= 0x04007FFF :
                 for i in range(0, 4):
                   data = data | (self.ram[int(dut.io_i_cmd_payload_pc and 0x00007FFF) + i] << (i*8))
               else:
                 raise TestFailure("out of range ICmd read")
 
               if self.wrapICmdZero and int(dut.io_i_cmd_payload_pc) == 0:
-                dut.io_i_rsp_payload_instruction <= 0xffc02e23; #01c02023   ffc02e23
+                dut.io_i_rsp_payload_instruction.value = 0xffc02e23; #01c02023   ffc02e23
               elif data == 0x00000073:
-                dut.io_i_rsp_payload_instruction <= 0xffc02e23;
+                dut.io_i_rsp_payload_instruction.value = 0xffc02e23;
               elif data == 0x0FF0000F:
-                dut.io_i_rsp_payload_instruction <= 0x00000013; #TODO remove me
+                dut.io_i_rsp_payload_instruction.value = 0x00000013; #TODO remove me
               else:
-                dut.io_i_rsp_payload_instruction <= data;
+                dut.io_i_rsp_payload_instruction.value = data;
 
     @cocotb.coroutine
     def driveDRsp(self):
         logg = open('log.txt', 'wb')
         dut = self.dut
-        dut.io_d_rsp_valid <= 0
+        dut.io_d_rsp_valid.value = 0
         counter = 0
         while True:
             yield RisingEdge(dut.clk)
             counter = counter + 1
             if int(dut.io_d_rsp_ready) == 1 :
-              dut.io_d_rsp_valid <= 0
+              dut.io_d_rsp_valid.value = 0
               randSignal(dut.io_d_rsp_payload)
 
             if int(dut.io_d_cmd_valid) == 1 and  int(dut.io_d_cmd_ready) == 1 :
@@ -130,12 +130,12 @@ class Tester:
                 elif int(dut.io_d_cmd_payload_address) == 0xFFFFFFFC :
                     pass
 
-                elif int(dut.io_d_cmd_payload_address) <= 0x03FFFFFF :
+                elif int(dut.io_d_cmd_payload_address.value) <= 0x03FFFFFF :
                   if not self.allowRomWrite:
                     raise TestFailure("Rom was written :(")
                   for i in range(0,1 << int(dut.io_d_cmd_payload_size)):
                     self.rom[int(dut.io_d_cmd_payload_address) + i] = (int(dut.io_d_cmd_payload_data) >> (i*8)) & 0xFF
-                elif int(dut.io_d_cmd_payload_address) <= 0x04007FFF :
+                elif int(dut.io_d_cmd_payload_address.value) <= 0x04007FFF :
                   # print("write %x %x" % (int(dut.io_d_cmd_payload_address),int(dut.io_d_cmd_payload_data)))
                   for i in range(0,1 << int(dut.io_d_cmd_payload_size)):
                       self.ram[(int(dut.io_d_cmd_payload_address) & 0x00007FFF) + i] = (int(dut.io_d_cmd_payload_data) >> (i*8)) & 0xFF
@@ -144,26 +144,26 @@ class Tester:
                     raise TestFailure("dCmd out of range %x" %(int(dut.io_d_cmd_payload_address)))
 
               else:
-                dut.io_d_rsp_valid <= 1
+                dut.io_d_rsp_valid.value = 1
                 if int(dut.io_d_cmd_payload_address) == 0xF0000040 :
-                  dut.io_d_rsp_payload <= counter
+                  dut.io_d_rsp_payload.value = counter
                 elif int(dut.io_d_cmd_payload_address) == 0xF0000020 :
-                  dut.io_d_rsp_payload <= 0
+                  dut.io_d_rsp_payload.value = 0
                 elif int(dut.io_d_cmd_payload_address) == 0xF0000000 :
-                  dut.io_d_rsp_payload <= 0
+                  dut.io_d_rsp_payload.value = 0
                 elif int(dut.io_d_cmd_payload_address) == 0xF0000004 :
-                  dut.io_d_rsp_payload <= 0xFFFF0000
-                elif int(dut.io_d_cmd_payload_address) <= 0x03FFFFFF :
+                  dut.io_d_rsp_payload.value = 0xFFFF0000
+                elif int(dut.io_d_cmd_payload_address.value) <= 0x03FFFFFF :
                     data = 0
                     for i in range(0,4):
                         data |= self.rom[int(dut.io_d_cmd_payload_address) + i] << (i*8)
-                    dut.io_d_rsp_payload <= data
-                elif int(dut.io_d_cmd_payload_address) <= 0x04007FFF :
+                    dut.io_d_rsp_payload.value = data
+                elif int(dut.io_d_cmd_payload_address.value) <= 0x04007FFF :
                     data = 0
                     for i in range(0, 4):
                         data |= self.ram[(int(dut.io_d_cmd_payload_address) & 0x00007FFF) + i]  << (i*8)
                     # print("read %x %x" % (int(dut.io_d_cmd_payload_address), int(dut.io_d_cmd_payload_data)))
-                    dut.io_d_rsp_payload <= data
+                    dut.io_d_rsp_payload.value = data
                 else:
                     raise TestFailure("dCmd out of range %x" %(int(dut.io_d_cmd_payload_address)))
 
