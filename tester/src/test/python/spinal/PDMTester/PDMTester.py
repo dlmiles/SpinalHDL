451bd03e56b02ba1d9c868af0e62f5b3075820d4
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-05-15 11:03:05 +0100

    WIP tester/python: assignment = is deprecated: PDMTester/PDMTester.py

--- tester/src/test/python/spinal/PDMTester/PDMTester.py
+++ tester/src/test/python/spinal/PDMTester/PDMTester.py
@@ -8,24 +8,24 @@ from cocotb.triggers import Timer, Edge, RisingEdge, Join, FallingEdge
 @cocotb.coroutine
 def genClock(dut, nCycles):
      for cycle in range(nCycles):
-        dut.clk = 0
+        dut.clk.value = 0
         yield Timer(10)
-        dut.clk = 1
+        dut.clk.value = 1
         yield Timer(10)
 
 @cocotb.coroutine
 def testDensity(dut, density):
-    dut.io_enable = 0
+    dut.io_enable.value = 0
     yield genClock(dut, 1)
-    dut.io_enable = 1
-    dut.io_density = density
+    dut.io_enable.value = 1
+    dut.io_density.value = density
     yield genClock(dut, 1)
     pulseCounter = 0
     for cycle in range(256):
-        dut.clk = 0
+        dut.clk.value = 0
         pulseCounter = pulseCounter + 1 if (dut.io_output == 1) else pulseCounter
         yield Timer(10)
-        dut.clk = 1
+        dut.clk.value = 1
         yield Timer(10)
     dut._log.info("Pulse Count : %s expected %s" % (str(pulseCounter),str(density)))
     if pulseCounter != density:
@@ -33,9 +33,9 @@ def testDensity(dut, density):
 
 @cocotb.test()
 def main_test(dut):
-    dut.reset = 1
+    dut.reset.value = 1
     yield genClock(dut, 2)
-    dut.reset = 0
+    dut.reset.value = 0
 
     for i in range(50):
         yield testDensity(dut, random.randint(0,256))
