adb248e0acb265dcc8ad71eb2bc59c97c3b05f00
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-05-15 11:26:36 +0100

    tester/python: ReturnValue is deprecated: common/CoreCom.py

--- tester/src/test/python/spinal/Pinsec/common/CoreCom.py
+++ tester/src/test/python/spinal/Pinsec/common/CoreCom.py
@@ -11,7 +11,7 @@ def readCoreValue(dut):
     while True:
         yield RisingEdge(dut.io_axiClk)
         if int(bridge.io_apb_PENABLE) == 1 and int(bridge.io_apb_PSEL) == 1 and int(bridge.io_apb_PWRITE) == 1 and int(bridge.io_apb_PADDR) == 0xFFF00:
-            raise ReturnValue(int(bridge.io_apb_PWDATA))
+            return int(bridge.io_apb_PWDATA)
 
 @cocotb.coroutine
 def readCoreValueAssert(dut,value,message):
