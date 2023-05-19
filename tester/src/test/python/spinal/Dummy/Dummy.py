bbd292de41d875d31ac66c0e8989282a190c6b7a
Darryl L. Miles <darryl.miles@darrylmiles.org>
2023-05-19 10:49:52 +0100

    SQUISH: Dummy.py

--- tester/src/test/python/spinal/Dummy/Dummy.py
+++ tester/src/test/python/spinal/Dummy/Dummy.py
@@ -1,11 +1,16 @@
+#
+#  This project is used by:
+#   source ./tool.sh
+#   purge_cocotb
+#
+# So the VPI interface is built ready tp run parallel unit testing
+#
 import cocotb
 from cocotb.triggers import Timer
-from cocotblib.misc import set_timeout
 
 
 @cocotb.test()
 def test1(dut):
-    set_timeout()
     dut._log.info("Cocotb test boot")
     yield Timer(1000)
     dut._log.info("Cocotb test done")
